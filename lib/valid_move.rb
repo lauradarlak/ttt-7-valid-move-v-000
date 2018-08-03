27770a68a07a4fe254b46950a3d67940afc11fab
Laura Darlak <laura@polyporedesign.com>
2018-08-03 19:56:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,22 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  move = nil
-  if position_taken?(board, index)
-    move = nil
-  elsif index.between?(0,8)
-    move = true
-  else
-    move = false
-  end
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
 end
+
+# def valid_move?(board, index)
+#   move = nil
+#   if position_taken?(board, index)
+#     move = nil
+#   elsif index.between?(0,8)
+#     move = true
+#   else
+#     move = false
+#   end
+# end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
